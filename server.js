4e0b4327339f362a5bbcbad0762264d858889245
dreamycodex <dreuphoricme@gmail.com>
2023-08-06 15:56:46 +0100

    fix cors issue

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+
 const products = require('./db/products');
 
 //creating a server
 
 const app = express();
+app.use=(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send("Hello from Express App!")
 })
 
-app.get("/api/products", (req, res) => {
+app.get('/api/products', (req, res) => {
     res.status(200).json(products);
 })
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log("server is up and running")
 });
 
