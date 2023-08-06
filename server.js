251c5c02e6b818bea8070d11fafba198d1e92014
dreamycodex <dreuphoricme@gmail.com>
2023-08-06 16:39:07 +0100

    install uuid and change port id

--- server.js
+++ server.js
@@ -6,18 +6,20 @@ const products = require('./db/products');
 //creating a server
 
 const app = express();
-app.use=(cors());
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Hello from Express App!")
+    res.send("Hello from Express Application!")
 })
+
+// getting all products 
 
 app.get('/api/products', (req, res) => {
     res.status(200).json(products);
 })
 
-app.listen(3001, () => {
+app.listen(3000, () => {
     console.log("server is up and running")
 });
 
